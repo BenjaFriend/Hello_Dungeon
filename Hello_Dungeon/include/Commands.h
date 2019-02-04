f53957cc12d890e1110d9de4f68429191419a7a4
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2019-02-04 10:23:05 -0500

    Update cmake and file strucutre

---
+++ Hello_Dungeon/include/Commands.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/// <summary>
+/// Common structure definitions of commands and other socket uses
+/// </summary>
+/// <author>Ben Hoffman</author>
+
+
+#define DEF_BUF_SIZE 256
---
+++ Hello_Dungeon/src/Main.cpp
@@ -0,0 +1,12 @@
+/// <summary>
+/// Entry pointer of the program, parses the command 
+/// line input and creates a client or server accordingly
+/// </summary>
+
+#include <iostream>
+
+int main( int argc, char* argv [] )
+{
+    std::cout << "Hello Dungeon!" << std::endl;
+    return 0;
+}
